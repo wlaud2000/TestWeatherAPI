700d6c0ad1bb62b5268cbf7a55aeae07353ad676
Jimyeong Kim <kjm021221@icloud.com>
2025-07-04 12:06:11 +0900

    🎉Begin a Project

---
+++ src/main/java/com/study/demo/testweatherapi/TestWeatherApiApplication.java
@@ -0,0 +1,13 @@
+package com.study.demo.testweatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWeatherApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestWeatherApiApplication.class, args);
+    }
+
+}
